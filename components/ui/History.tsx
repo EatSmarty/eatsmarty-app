b044965a9240e64836bd83097de2db782798427c
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2025-02-01 04:04:06 +0330

    update code

--- components/ui/History.tsx
+++ components/ui/History.tsx
@@ -7,7 +7,7 @@ function History() {
   return (
     <View style={styles.container}>
         <Text variant='titleLarge'>History</Text>
-        <View style={styles.iconContainer}>
+        <View>
             {/* <MaterialCommunityIcons name='history' size={50} color="#333" /> */}
         </View>
     </View>
@@ -19,16 +19,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     paddingTop: 20,
     // paddingHorizontal: 10
-  },
-  iconContainer: {
-    width: 100, 
-    height: 100,
-    display: 'flex',
-    borderRadius: 50,
-    backgroundColor: 'red',
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 20, 
   }
 });
 
