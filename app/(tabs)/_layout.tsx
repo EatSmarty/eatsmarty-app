1204d67fca94efc93ca57313b5ddf319be7973dd
Hosein Ghasemizade <ghasemizade.hosein@gmail.com>
2025-02-04 04:33:05 +0330

    fix ui bottom navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import { Tabs } from 'expo-router';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Layout() {
   return (
     <Tabs>
       <Tabs.Screen 
         name="index" 
-        options={{ title: 'Home' }} />
+        options={{ 
+          title: 'Eatsmarty',
+          tabBarActiveTintColor: "#18DAA3",
+          tabBarIcon: () => <Icon name="home" size={20} color="#18DAA3" />
+         }} />
       <Tabs.Screen 
         name="scan" 
-        options={{ title: 'Scan' }} />
+        options={{ 
+            title: 'Scan',
+            tabBarActiveTintColor: "#18DAA3",
+            tabBarIcon: () => <Icon name="qrcode" size={20} color="#18DAA3" />
+          }} />
       <Tabs.Screen 
         name="(additives)"
-        options={{ title: 'Additive Details', headerShown: false }} />
+        options={{ 
+            title: 'Additive Details',
+            headerShown: false,
+            tabBarActiveTintColor: "#18DAA3",
+            tabBarIcon: () => <Icon name="bars" size={20} color="#18DAA3" />
+          }} />
     </Tabs>
   );
 }
